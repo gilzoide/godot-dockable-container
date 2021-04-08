3652715c4e4597fbde31b404064b011f0ed4169c
gilzoide <gilzoide@gmail.com>
2021-04-07 21:24:56 -0300

    Fix editor plugin reset value

--- addons/dockable_container/inspector_plugin/layout_editor_property.gd
+++ addons/dockable_container/inspector_plugin/layout_editor_property.gd
@@ -1,6 +1,8 @@
 extends EditorProperty
 
 const DockableContainer = preload("res://addons/dockable_container/dockable_container.gd")
+const Layout = preload("res://addons/dockable_container/layout.gd")
+const LayoutRoot = preload("res://addons/dockable_container/layout_root.gd")
 
 var _container = DockableContainer.new()
 
@@ -22,26 +24,29 @@ func _ready() -> void:
 		new_control.valign = Label.VALIGN_CENTER
 		new_control.text = child.name
 		_container.add_child(new_control)
-	var current_value = original_container.get(get_edited_property())
-	if current_value == null:
-		original_container.set(get_edited_property(), DockableContainer.LayoutRoot.new())
-	_container.set(get_edited_property(), original_container.get(get_edited_property()))
-	_container.queue_sort()
+
+
+func update_property() -> void:
+	var original_container: DockableContainer = get_edited_object()
+	var value = original_container.get(get_edited_property())
+	if value == null:
+		value = LayoutRoot.new()
+		value.root = Layout.LayoutPanel.new()
+		original_container.set(get_edited_property(), value)
+		original_container._update_tree_indices()
+		value.connect("changed", original_container, "queue_sort")
+		value.connect("changed", _container, "queue_sort")
+	_container.set(get_edited_property(), value)
+	_container._update_tree_indices()
 
 
 func _on_child_tab_selected(_child_index: int) -> void:
 	emit_changed(get_edited_property(), _container.get(get_edited_property()))
 	var original_container: DockableContainer = get_edited_object()
-	original_container.call_deferred("queue_sort")
+	original_container.queue_sort()
 
 
 func _on_layout_changed() -> void:
 	emit_changed(get_edited_property(), _container.get(get_edited_property()))
 	var original_container: DockableContainer = get_edited_object()
-	original_container.queue_sort()
-
-
-func update_property() -> void:
-	var original_container: DockableContainer = get_edited_object()
-	_container.set(get_edited_property(), original_container.get(get_edited_property()))
 	original_container.queue_sort()
