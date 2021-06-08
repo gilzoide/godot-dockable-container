be47b399d9f0b93a27ccb874c2b222853790517f
gilzoide <gilzoide@gmail.com>
2021-06-08 11:10:08 -0300

    Remove DockableContainerLayout custom Resource type (#2)
    It is not needed and clutters exported Resource menus on inspector

--- addons/dockable_container/plugin.gd
+++ addons/dockable_container/plugin.gd
@@ -2,18 +2,15 @@ tool
 extends EditorPlugin
 
 const DockableContainer = preload("dockable_container.gd")
-const Layout = preload("layout.gd")
 const LayoutInspectorPlugin = preload("inspector_plugin/editor_inspector_plugin.gd")
 
 var _layout_inspector_plugin = LayoutInspectorPlugin.new()
 
 func _enter_tree() -> void:
 	add_custom_type("DockableContainer", "Container", DockableContainer, null)
-	add_custom_type("DockableContainerLayout", "Resource", Layout, null)
 	add_inspector_plugin(_layout_inspector_plugin)
 
 
 func _exit_tree() -> void:
 	remove_inspector_plugin(_layout_inspector_plugin)
-	remove_custom_type("DockableContainerLayout")
 	remove_custom_type("DockableContainer")
