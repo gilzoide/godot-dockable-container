de6d7f0cf6ca90878054992ab193f7ecccf8a5d6
烟汐忆梦_YM <140093497+Yanxiyimengya@users.noreply.github.com>
2026-02-16 05:33:28 +0800

    Fixed compatibility issues when dragging the TabBar in odot 4.6.

--- addons/dockable_container/dockable_container.gd
+++ addons/dockable_container/dockable_container.gd
@@ -237,13 +237,18 @@ func get_tab_count() -> int:
 
 
 func _can_handle_drag_data(data) -> bool:
-	if data is Dictionary and data.get("type") in ["tab_container_tab", "tabc_element"]:
-		var tabc := get_node_or_null(data.get("from_path"))
-		return (
-			tabc
-			and tabc.has_method("get_tabs_rearrange_group")
-			and tabc.get_tabs_rearrange_group() == rearrange_group
-		)
+	if not data is Dictionary: return false
+	var type = data.get("type", "")
+	var tab_type = data.get("tab_type", "")
+	var is_valid_type = \
+			type in ["tab", "tab_container_tab", "tabc_element"] or \
+			tab_type in ["tab_container_tab", "tabc_element"]
+	if is_valid_type:
+		var from_path = data.get("from_path")
+		if from_path:
+			var source_node = get_node_or_null(from_path)
+			if source_node and source_node.has_method("get_tabs_rearrange_group"):
+				return source_node.get_tabs_rearrange_group() == rearrange_group
 	return false
 
 
